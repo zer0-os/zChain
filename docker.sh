db3efb35c9ecb8498ffc8d23c61dc576c46bf206
ratik21 <ratikjindal21@gmail.com>
2022-02-07 17:17:50 +0530

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,6 @@
 docker system prune -f # cleanup
+docker container stop zchain
+docker container rm zchain
 docker build . -t zchain
-docker start --name zchain -p 3000:3000 -dit zchain:latest
+docker run --name zchain -p 3000:3000 -dit zchain:latest
 docker exec -it zchain /bin/bash
