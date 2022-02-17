671e2b87d6471b83d5469c1eaeef8a037bf77406
ratik21 <ratikjindal21@gmail.com>
2022-02-17 10:56:42 +0530

    changes to meow app

--- apps/meow/src/app.ts
+++ apps/meow/src/app.ts
@@ -3,10 +3,13 @@ import { MEOW } from "./meow";
 
 ;(async () => {
   const meow = new MEOW();
-  await meow.init('n30.json'); // initializes local zchain
+  await meow.init('ratik.json'); // initializes local zchain
 
-//   // tweet by meow
-//   meow.tweet(`Thrilled to join the football gaming revolution
-//  with @UFLgame bringing the new #fairtoplay experience to
-//  football gamers worldwide! See you in the Game soon! #ucl`);
+  // 5s delay for discovery and connection of other peers
+  await delay(5 * 1000);
+
+  // tweet by meow
+  meow.sendMeow(`Thrilled to join the football gaming revolution
+ with @UFLgame bringing the new #fairtoplay experience to
+ football gamers worldwide! See you in the Game soon! #ucl`);
 })();
