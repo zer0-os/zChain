9bf50e12146867110bfa5695ed3f7be615015d5b
bustawei <97470400+bustawei@users.noreply.github.com>
2022-04-20 20:59:01 +0000

    add chat #2

--- examples/zchain/chainOps/zScreen.js
+++ examples/zchain/chainOps/zScreen.js
@@ -11,15 +11,17 @@ export class ZScreen {
 	        this.screen.title = "Zchain"
 		this.screen.key(['escape',  'C-c'], function(ch, key) {return process.exit(0);});
 		this.grid = new contrib.grid({rows: 12, cols: 12, screen: this.screen});
-		this.choices = ["Profile","Connections","Settings","Topics feed"]
+		this.choices = ["Profile","Verified nodes","Chat"]
 		this.meowAsci = fs.readFileSync("meowAsci.txt")
 		this.verifiedNodesList=blessed.list({});
 		this.initStaticScreen()
+		this.topicsBox = blessed.list({})
+		this.topicChatForm = blessed.form({})
 	}
 	initStaticScreen(){
 		this.mapBox = this.grid.set(6,0,6,6,contrib.map,{label:"Nodes map"});
 		this.connectionsLogBox = this.grid.set(9,6,3,6,contrib.log,{fg:"green",selectedFg:"green",label:"Discovery and connections"});
-		this.subscribedTopicsLog = this.grid.set(6,6,3,6,blessed.log,{fg:"green",selectedFg:"green",label:"Subscribed topics feed"});
+		this.subscribedTopicsLog = this.grid.set(6,6,3,6,blessed.log,{fg:"green",selectedFg:"green",label:"Console logs"});
 		this.choiceListBox = this.grid.set(0,0,6,2,blessed.list,{
 			interactive:true,
 			mouse: true,
@@ -37,12 +39,98 @@ export class ZScreen {
 		this.drawProfileBox();
 
 	}
+	drawTopicChatBox(topicName){
+		this.topicChatBox = this.grid.set(0,2,6,6,blessed.box,{
+			keys:true,
+                        label:topicName,
+			name: topicName,
+                        mouse:true
+		});
+		this.topicChatLogs = blessed.log({
+			fg:"green",
+			parent: this.topicChatBox,
+			height:"70%",
+		})
+		this.topicChatNew = blessed.textbox({
+                        parent:this.topicChatBox,
+                        left:"5%",
+                        inputOnFocus: true,
+                        width:"70%",
+                        top:"80%",
+                        fg:"blue",
+                        focus:{fg:"blue"},
+                        border:{type:"line"},
+                        shrink:true,
+                        value:""
+                });
+                this.topicChatNew.on("click",function(){
+
+                });
+
+		this.submitTopicChatButton = blessed.button({
+                        parent: this.topicChatBox,
+                        mouse:true,
+                        keys:true,
+                        shrink:true,
+                        content:"Send meow",
+                        style: {bg: 'black',focus: {bg: 'blue'},hover: {bg: 'blue'},fg:"white"},
+                        padding: {left: 1,right: 1},
+                        left:"80%",
+                        top:"85%"
+                });
+
+	}
+	//topics Box
+	drawTopicsBox(topics){
+		this.topicsWrapper = this.grid.set(0,2,6,6,blessed.box,{label:"Topics dashboard"})
+		this.topicsBox = blessed.list({
+			parent:this.topicsWrapper,
+                        mouse:true,
+                        align:"center",
+                        selectedFg:"blue",
+                        selectedBg:"green",
+                        fg:"white",
+			height:"70%",
+                        items:topics,
+                        interactive:true,
+			keys:true
+
+		});
+		this.topicCreateName = blessed.textbox({
+                        parent:this.topicsWrapper,
+                        left:"5%",
+                        inputOnFocus: true,
+                        width:"70%",
+                        top:"80%",
+                        fg:"blue",
+                        focus:{fg:"blue"},
+                        border:{type:"line"},
+                        shrink:true,
+                        value:""
+                });
+                this.topicCreateName.on("click",function(){
+
+                });
+
+                this.submitTopicCreateButton = blessed.button({
+                        parent: this.topicsWrapper,
+                        mouse:true,
+                        keys:true,
+                        shrink:true,
+                        content:"Create topic",
+                        style: {bg: 'black',focus: {bg: 'blue'},hover: {bg: 'blue'},fg:"white"},
+                        padding: {left: 1,right: 1},
+                        left:"80%",
+                        top:"85%"
+                });
+
+	}
 	//znas owned by a node
 	drawOwnedZnasBox(ownedDomains){
 		var formattedZnas=[]
 		ownedDomains.forEach(zna=>{
 			var lastIndexOfPoint = zna.name.lastIndexOf(".")
-			formattedZnas.push([zna.name.substring(0,lastIndexOfPoint),zna.name.substring(lastIndexOfPoint+1),new bigInt(zna.id.substring(2),16)])
+			formattedZnas.push([zna.name.substring(zna.name.indexOf(".")+1,lastIndexOfPoint),zna.name.substring(lastIndexOfPoint+1),new bigInt(zna.id.substring(2),16)])
 		});
 		if(formattedZnas.length>0)
 		{
