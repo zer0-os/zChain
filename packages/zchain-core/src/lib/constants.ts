af739691729d2c64ee96a4164534e465129318d9
ratik21 <ratikjindal21@gmail.com>
2022-05-13 05:00:40 +0530

    revert

--- packages/zchain-core/src/lib/constants.ts
+++ packages/zchain-core/src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const RELAY_ADDRS = [
   // custom relay servers (on aws ec2)
   '/ip4/54.185.175.99/tcp/15003/ws/p2p/QmQ413oYwG8n7exFHS9ZywWzYndcyUAwWpxgBsUvvRxQ5q',
-  '/ip4/18.236.202.152/tcp/15003/ws/p2p/QmVa4AaHxHU9VrMXJ4bVsBM3FyKJHKpCeNZXJH6ojbzfiE',
+  //'/ip4/18.236.202.152/tcp/15003/ws/p2p/QmVa4AaHxHU9VrMXJ4bVsBM3FyKJHKpCeNZXJH6ojbzfiE',
   // '/ip4/54.185.175.99/tcp/33511/ws/p2p/QmTNbBFdjzTrJSXtr4ASkc4EWmatfA6HS4kanLCm5PeP49',
   // '/ip4/54.185.175.99/tcp/40691/ws/p2p/QmWvc9FJKgm5zUMJ4yoa4pgFQacCGNBjE9wuDi8RB4ZYrX',
   // '/ip4/54.185.175.99/tcp/46815/ws/p2p/QmZZAvdi84d5ETgtYg292HWprfmPvzRj84ZTCfJPsvMgpG',
