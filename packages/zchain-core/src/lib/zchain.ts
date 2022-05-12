b4b29245378e9a99c5b78a75a7e72c2be7c10fa1
ratik21 <ratikjindal21@gmail.com>
2022-05-13 05:29:06 +0530

    remove from bootstrapp

--- packages/zchain-core/src/lib/zchain.ts
+++ packages/zchain-core/src/lib/zchain.ts
@@ -86,7 +86,7 @@ export class ZCHAIN {
             Swarm: [],
           },
           Bootstrap: [
-            ...RELAY_ADDRS
+            //...RELAY_ADDRS
           ],
           "Swarm": {
               "ConnMgr": {
@@ -126,9 +126,9 @@ export class ZCHAIN {
       console.log("\n★ ", chalk.cyan('zChain Node Activated: ' + node.peerId.toB58String()) + " ★\n");
       this.node = node;
 
-      // for (const r of RELAY_ADDRS) {
-      //   try { await this.ipfs.swarm.connect(r); } catch(e) {}
-      // }
+      for (const r of RELAY_ADDRS) {
+        try { await this.ipfs.swarm.connect(r); } catch(e) {}
+      }
 
       // intialize zstore
       this.zStore = new ZStore(this.ipfs, this.node, password);
