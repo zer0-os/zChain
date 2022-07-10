475d9367ec13640947d11b5191c2063d97eb3bd8
ratik21 <ratikjindal21@gmail.com>
2022-07-11 02:42:04 +0530

    update meow types

--- apps/meow/src/types.ts
+++ apps/meow/src/types.ts
@@ -1,11 +1,24 @@
 import { types as ztypes } from "zchain-core";
 import KeyValueStore from "orbit-db-kvstore";
+import * as Y from 'yjs';
 
 export interface MeowDBs {
   channels: ztypes.FeedMap
   followingZIds: KeyValueStore<unknown>
   followingChannels: KeyValueStore<unknown>
   networks: KeyValueStore<unknown>
+}
+
+// meow public yDocs (eg. network info is shared among everyone)
+export interface MeowPublicYDocs {
+  doc: Y.Doc // root y.doc
+  networks: Y.Map<unknown>
+}
+
+export interface MeowPrivateYDocs {
+  doc: Y.Doc // root y.doc
+  followingZIds: Y.Map<unknown>
+  followingChannels: Y.Map<unknown>
 }
 
 export interface TwitterConfig {
