b7ccef9ef8aa7c8e70f2cd0aff1ef6dce6db8c23
ratik21 <ratikjindal21@gmail.com>
2022-07-26 11:07:03 +0530

    update

--- terraform/0-ec2-ssh.sh
+++ terraform/0-ec2-ssh.sh
@@ -1,4 +1,2 @@
-ssh -t -i ~/terraform-us-east-2-demo.pem ubuntu@3.23.87.165 -o StrictHostKeyChecking=no 'cd zChain/terraform;sudo sh 1-start-node.sh;command;/bin/bash'
 
-# cd zChain
-# cat install.sh
+ssh -t -i ~/terraform-us-east-2-demo.pem ubuntu@3.23.87.165 -o StrictHostKeyChecking=no 'cd zChain/; git checkout terraform-simulator; cd terraform/;sudo sh 1-start-node.sh;command;/bin/bash'
