9a795244a46188b38d410082c6d80ec0e192373b
Ratik Jindal <ratikjindal21@gmail.com>
2022-08-29 17:39:22 +0530

    add more regions in terraform

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,6 +3,7 @@ variable "ec2_ami" {
 
   default = {
     us-east-2 = "ami-02f3416038bdb17fb"
+    #us-east-1 = "ami-052efd3df9dad4825"
     #us-west-1 = "ami-085284d24fe829cd0"
   }
 }
@@ -17,4 +18,6 @@ variable "region" {
 # Creating a Variable for instance_type
 variable "instance_type" {
   type = string
+
+  default = "t2.micro"
 }
