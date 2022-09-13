287b2d019309497b5216e9ae66b957e69957bae6
Ratik Jindal <ratikjindal21@gmail.com>
2022-09-13 17:55:53 +0530

    feat: add more providers in providors.tf

--- simulator/nodes/provider.tf
+++ simulator/nodes/provider.tf
@@ -16,3 +16,48 @@ provider "aws" {
   region  = "us-west-2"
   alias   = "us-west-2"
 }
+
+provider "aws" {
+  region  = "af-south-1"
+  alias   = "af-south-1"
+}
+
+provider "aws" {
+  region  = "ap-east-1"
+  alias   = "ap-east-1"
+}
+
+provider "aws" {
+  region  = "ap-southeast-3"
+  alias   = "ap-southeast-3"
+}
+
+provider "aws" {
+  region  = "ap-southeast-2"
+  alias   = "ap-southeast-2"
+}
+
+provider "aws" {
+  region  = "ap-southeast-1"
+  alias   = "ap-southeast-1"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  alias   = "ap-south-1"
+}
+
+provider "aws" {
+  region  = "ap-northeast-3"
+  alias   = "ap-northeast-3"
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+  alias   = "ap-northeast-2"
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  alias   = "ap-northeast-1"
+}
