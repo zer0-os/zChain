1e494a5799d3b82e76d3d88e7f31eb7bf3dd38e9
Ratik Jindal <ratikjindal21@gmail.com>
2022-09-28 23:42:48 +0530

    chore: update

--- apps/meow/src/test-scripts/ratik.ts
+++ apps/meow/src/test-scripts/ratik.ts
@@ -5,7 +5,7 @@ import { MEOW } from "../meow";
   const meow = new MEOW();
   await meow.init('ratik'); // initializes local zchain at ~/.zchain/ipfs/ratik
 
-  // // 5s delay for discovery and connection of other peers
+  // 5s delay for discovery and connection of other peers
   await delay(5 * 1000);
 
   // add channels in network
@@ -14,23 +14,23 @@ import { MEOW } from "../meow";
 
   await meow.followChannel('#ucl');
 
-  // tweet by meow
-  await meow.sendMeow(`This is first message #ucl`);
+  // // tweet by meow
+  // await meow.sendMeow(`This is first message #ucl`);
 
-  await delay(2 * 1000);
-  await meow.sendMeow(`This is second message #ucl`);
+  // await delay(2 * 1000);
+  // await meow.sendMeow(`This is second message #ucl`);
 
-  await delay(2 * 1000);
-  await meow.sendMeow(`This is third message #programming`);
+  // await delay(2 * 1000);
+  // await meow.sendMeow(`This is third message #programming`);
 
 
-  // let ctr = 0;
-  // setInterval(async () => {
-  //   const message = `This is message ${++ctr} #zero`;
+  let ctr = 0;
+  setInterval(async () => {
+    const message = `This is message ${++ctr} #zero`;
 
-  //   console.log("message: ", message);
-  //   await meow.sendMeow(message);
-  // }, 4000);
+    console.log("message: ", message);
+    await meow.sendMeow(message);
+  }, 10 * 1000);
 })()
   .catch(err => {
     console.log('E ', err);
