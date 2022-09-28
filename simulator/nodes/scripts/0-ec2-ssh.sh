0e5eedd0ee6af04eb0a9a12fd38525444cd9ec6b
Ratik Jindal <ratikjindal21@gmail.com>
2022-09-28 17:49:46 +0530

    feat: update run simulator script

--- simulator/nodes/scripts/0-ec2-ssh.sh
+++ simulator/nodes/scripts/0-ec2-ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# $1 == file name of .pem file (key)
+# $1 == file name of .pem/.cer file (key)
 # $2 == instance public ip
 
 chmod 400 ./keys/$1
-ssh -t -i ./keys/$1 ubuntu@$2 -o StrictHostKeyChecking=no 'cd zChain/; git pull;git checkout terraform-simulator; cd terraform/;sudo sh ./scripts/1-start-node.sh;command;/bin/bash'
+ssh -t -i ./keys/$1 ubuntu@$2 -o StrictHostKeyChecking=no 'cd zChain/; git pull;git checkout custom-script-terraform; cd simulator/nodes/;sudo sh ./scripts/1-start-node.sh;command;/bin/bash'
