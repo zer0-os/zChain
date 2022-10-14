d983ce5ad6aa6b37717940b77410933322a4f000
Ratik Jindal <ratikjindal21@gmail.com>
2022-10-14 16:53:36 +0530

    update

--- packages/zchain-core/src/lib/zchain.ts
+++ packages/zchain-core/src/lib/zchain.ts
@@ -53,8 +53,9 @@ export class ZCHAIN {
             //'/ip4/0.0.0.0/tcp/0',
             //'/ip4/0.0.0.0/tcp/0/ws',
             // // custom deployed webrtc-star signalling server
-            '/dns4/vast-escarpment-62759.herokuapp.com/tcp/443/wss/p2p-webrtc-star/',
-            '/dns4/sheltered-mountain-08581.herokuapp.com/tcp/443/wss/p2p-webrtc-star/',
+            // '/dns4/vast-escarpment-62759.herokuapp.com/tcp/443/wss/p2p-webrtc-star/',
+            // '/dns4/sheltered-mountain-08581.herokuapp.com/tcp/443/wss/p2p-webrtc-star/',
+            '/ip4/13.59.200.44/tcp/13579/ws/p2p-webrtc-star/',
             ...listenAddrs
           ]
         },
