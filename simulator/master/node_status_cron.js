e77e8caa9f3727bd2ea35e16504ec511d648e9fc
Ratik Jindal <ratikjindal21@gmail.com>
2022-10-17 15:49:02 +0530

    feat: add node_status_cron script:

---
+++ simulator/master/node_status_cron.js
@@ -0,0 +1,48 @@
+const { MongoClient } = require("mongodb");
+
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://ratik21:Qazxcvbn%401234@cluster0.0wfupod.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    console.log("Connected to Mongo");
+
+    const collection = client
+      .db("ZChain")
+      .collection("network");
+    const collections = await collection.find().toArray();
+
+    for (const c of collections) {
+      const currentTs =  Math.floor((new Date()).getTime() / 1000);
+      const lastTs = c["timestamp"];
+      if (!lastTs) { continue; }
+      
+      let status = ''; // online or offline or inactive
+      const weekInSeconds = 604800; // 7 * 24 * 60 * 60
+      if (currentTs - lastTs >= weekInSeconds) {
+        status = 'inactive';
+      } else if (currentTs - lastTs >= 41) { // if node hasn't sent a msg in 40s, then mark offline
+        status = 'offline';
+      } else {
+        //status = 'online';
+      }
+
+      if (status === '') { continue; }
+
+      await collection.findOneAndUpdate(
+        { "peerId": c["peerId"] }, // filter
+        { $set: { status: status } } // atomic operation req. for update
+      );
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main().catch(console.error);
