b6e126acf7171b798bc127b7a61e9711bcf31d1f
Ratik Jindal <ratikjindal21@gmail.com>
2022-10-18 10:08:54 +0530

    chore: update

--- simulator/nodes/scripts/0-ec2-ssh.sh
+++ simulator/nodes/scripts/0-ec2-ssh.sh
@@ -4,4 +4,4 @@
 # $2 == instance public ip
 
 chmod 400 ./keys/$1
-ssh -o IdentitiesOnly=yes -t -i ./keys/$1 ubuntu@$2 -o StrictHostKeyChecking=no 'cd zChain/; git pull;git checkout custom-script-terraform; cd simulator/nodes/;sudo sh ./scripts/1-start-node.sh;command;/bin/bash'
+ssh -o IdentitiesOnly=yes -t -i ./keys/$1 ubuntu@$2 -o StrictHostKeyChecking=no 'cd zChain/; git pull;git checkout before-esm; cd simulator/nodes/;sudo sh ./scripts/1-start-node.sh;command;/bin/bash'
