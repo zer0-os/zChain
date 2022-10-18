ade9dc2ca7da32b85b7f9cca10bc22d014b264e9
Ratik Jindal <ratikjindal21@gmail.com>
2022-10-18 09:38:17 +0530

    update node status cron

--- simulator/master/node_status_cron.js
+++ simulator/master/node_status_cron.js
@@ -2,7 +2,36 @@ const { MongoClient } = require("mongodb");
 const delay = require("delay");
 require('dotenv').config()
 
-async function updateNodeStatus() {
+async function updateNodeStatus(client) {
+  const collection = client
+    .db(process.env.DB_NAME)
+    .collection(process.env.COLLECTION_NAME);
+  const collections = await collection.find().toArray();
+  for (const c of collections) {
+    const currentTs =  Math.floor((new Date()).getTime() / 1000);
+    const lastTs = c["timestamp"];
+    if (!lastTs) { continue; }
+    
+    let status = ''; // online or offline or inactive
+    const weekInSeconds = 604800; // 7 * 24 * 60 * 60
+    if (currentTs - lastTs >= weekInSeconds) {
+      status = 'inactive';
+    } else if (currentTs - lastTs >= 41) { // if node hasn't sent a msg in 40s, then mark offline
+      status = 'offline';
+    } else {
+      //status = 'online';
+    }
+
+    if (status === '') { continue; }
+
+    await collection.findOneAndUpdate(
+      { "peerId": c["peerId"] }, // filter
+      { $set: { status: status } } // atomic operation req. for update
+    );
+  }
+}
+
+async function main() {
   /**
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
@@ -14,46 +43,18 @@ async function updateNodeStatus() {
     await client.connect();
     console.log("Connected to Mongo");
 
-    const collection = client
-      .db(process.env.DB_NAME)
-      .collection(process.env.COLLECTION_NAME);
-    const collections = await collection.find().toArray();
-
-    for (const c of collections) {
-      const currentTs =  Math.floor((new Date()).getTime() / 1000);
-      const lastTs = c["timestamp"];
-      if (!lastTs) { continue; }
-      
-      let status = ''; // online or offline or inactive
-      const weekInSeconds = 604800; // 7 * 24 * 60 * 60
-      if (currentTs - lastTs >= weekInSeconds) {
-        status = 'inactive';
-      } else if (currentTs - lastTs >= 41) { // if node hasn't sent a msg in 40s, then mark offline
-        status = 'offline';
-      } else {
-        //status = 'online';
-      }
-
-      if (status === '') { continue; }
-
-      await collection.findOneAndUpdate(
-        { "peerId": c["peerId"] }, // filter
-        { $set: { status: status } } // atomic operation req. for update
-      );
+    while(true) {
+      await updateNodeStatus(client);
+      console.log('Done\n\n'); // add some space b/w next run
+  
+      // wait 15 seconds before running
+      await delay(15 * 1000);
     }
-  } catch (e) {
+  }
+  catch (e) {
     console.error(e);
   }
-}
-
-async function main() {
-	while(true) {
-		await updateNodeStatus();
-		console.log('Done\n\n'); // add some space b/w next run
-
-		// wait 15 seconds before running
-		await delay(15 * 1000);
-	}				
+			
 }
 
 main();
