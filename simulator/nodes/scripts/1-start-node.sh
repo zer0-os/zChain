979891184af3f4a7e2909ffe869e903f7dc6be42
Ratik Jindal <ratikjindal21@gmail.com>
2022-11-14 00:40:36 +0530

    revert

--- simulator/nodes/scripts/1-start-node.sh
+++ simulator/nodes/scripts/1-start-node.sh
@@ -1,9 +1,6 @@
 # starts zchain node at an ec2 launched by terraform
 # note: this scripts runs "inside" the ec2 (after ssh)
 
-export LC_ALL="en_US.UTF-8"
-sudo dpkg-reconfigure locales
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
