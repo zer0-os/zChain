9f818386f660632abe066c32c2db43f43d0a7f5a
Ratik Jindal <ratikjindal21@gmail.com>
2022-11-14 00:12:15 +0530

    test

--- simulator/nodes/scripts/1-start-node.sh
+++ simulator/nodes/scripts/1-start-node.sh
@@ -1,11 +1,16 @@
 # starts zchain node at an ec2 launched by terraform
 # note: this scripts runs "inside" the ec2 (after ssh)
 
+export LC_ALL="en_US.UTF-8"
+sudo dpkg-reconfigure locales
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 nvm use node
 npm install -g yarn
+
+
 cd ../../
 sh install.sh
 cd apps/meow
