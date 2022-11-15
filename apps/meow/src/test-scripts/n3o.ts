252d11952dbd5f0456d5d5d965372fce55db1625
Ratik Jindal <ratikjindal21@gmail.com>
2022-11-16 01:58:34 +0530

    remove simulator

--- apps/meow/src/test-scripts/n3o.ts
+++ apps/meow/src/test-scripts/n3o.ts
@@ -3,46 +3,30 @@ import { MEOW } from "../meow";
 
 ;(async () => {
   const meow = new MEOW();
-  await meow.init('n3o');// initializes local zchain at ~/.zchain/ipfs/n3o
+  await meow.init('n3o'); // initializes local zchain at ~/.zchain/ipfs/ratik
 
-  // // 3s delay for discovery and connection of other peers
-  // await delay(3 * 1000);
+  // 5s delay for discovery and connection of other peers
+  await delay(5 * 1000);
 
-  // add channels in network
-  await meow.addChannelInNetwork('0://default.network', '#ucl');
-  await meow.addChannelInNetwork('0://default.network', '#fairtoplay');
 
-  
-  // tweet by meow
-  await meow.sendMeow(`Thrilled to join the football gaming revolution
-  with @UFLgame bringing the new #fairtoplay experience to
-  football gamers worldwide! See you in the Game soon! #ucl`);
+  // // tweet by meow
+  // await meow.sendMeow(`This is first message #ucl`);
 
-  /********    Following a peer    *********/
-  const ratikId = 'QmTsUsXsRsUpvHxRNXWJKmw3RvSPN3c8Noa95Kpduu5Wcv';
+  // await delay(2 * 1000);
+  // await meow.sendMeow(`This is second message #ucl`);
 
-  // follow ratik
-  await meow.followZId(ratikId);
-  meow.getFollowedPeers();
+  // await delay(2 * 1000);
+  // await meow.sendMeow(`This is third message #programming`);
 
-  // display last 5 messages posted by ratik
-  await delay(6 * 1000);
-  await meow.getPeerFeed(ratikId, 5);
 
-  /********    Following a channel    *********/
+  let ctr = 0;
+  setInterval(async () => {
+    const message = `This is message ${++ctr} #zero`;
 
-  await meow.followChannel('#fairtoplay');
-
-  meow.getFollowedChannels();
-
-  await delay(6 * 1000);
-  await meow.getChannelFeed('#fairtoplay', 3); // display last 3 messages on channel #ucl
-
-  // setInterval(async () => {
-  //   console.log("P: ", await meow.zchain.ipfs.swarm.peers());
-  // }, 4000);
-
+    console.log("message: ", message);
+    await meow.sendMeow(message);
+  }, 20 * 1000);
 })()
   .catch(err => {
-    console.log('E ', err)
+    console.log('E ', err);
     throw new Error(err); });
--- simulator/nodes/scripts/0-ec2-ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# $1 == file name of .pem/.cer file (key)
-# $2 == instance public ip
-
-chmod 400 ./keys/$1
-ssh -o IdentitiesOnly=yes -i ./keys/$1 ubuntu@$2 -o StrictHostKeyChecking=no 'bash -s' < ./scripts/1-start-node.sh
